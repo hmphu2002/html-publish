a54fac507604f5f28c0ba640097436614e900d8e
hmphu2002 <hmphu2002@gmail.com>
2024-05-12 13:25:36 +0000

    new file:   .env 	new file:   css/styles.css 	new file:   docker-compose.yml 	new file:   favicon.ico 	new file:   favicon.png 	new file:   fonts/fontawesome.ttf 	new file:   index.html 	new file:   nginx.conf 	new file:   submit.php 	new file:   template/1.html

---
+++ submit.php
@@ -0,0 +1,10 @@
+<?php
+$t=time();
+$txt = $_POST["textareaCode"];
+$fileName = $_POST["fileName"];
+$myfile = fopen("template/".$fileName.".html", "w") or die("Unable to open file!");
+fwrite($myfile, $txt);
+fclose($myfile);
+header("Location: /template/".$fileName);
+exit();
+?>
